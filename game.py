6341ce93ebc2d4e11479aa5553ecfe8e71c08b3f
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-05-12 19:41:14 -0300

    'pipe()' method improved

--- game.py
+++ game.py
@@ -8,20 +8,28 @@ class Game:
         self.screen_size = (400, 600)
         self.screen = pygame.display.set_mode(self.screen_size)
         pygame.display.set_caption('Flappy Bird')
-        self.clock = pygame.time.Clock()
-        self.sizes = {'Bird':(34, 24), 'Pipe':(52, 620)}
+
+        self.img_sizes = {'Bird':(34, 24), 'Pipe':(52, 620)}
         self.score_img = {}
         self.clock = pygame.time.Clock()
-        self.ground, self.roof = 576, 0
-        self.x_bird, self.y_bird = 150.0, 300.0
-        self.x_pipe, self.y_pipe = 350, random.randint(170, 530)
-        self.x_second_pipe, self.y_second_pipe = 576, random.randint(150, 550)
-        self.space_between_pipes = 100
+        self.ground, self.roof = self.screen_size[1]-self.img_sizes['Bird'][1], 0
+
+        self.space_between_pipes = 110 # Space between top and bottom pipes
+        self.pipe_limits = 50 # Boundary to blit pipes
+        # Upper and lower bound limits to pass to randint
+        self.rand_low = self.space_between_pipes + self.pipe_limits
+        self.rand_high = self.screen_size[1] - self.pipe_limits
+
+        self.x_bird, self.y_bird = self.screen_size[0]/3.0, self.screen_size[1]/2.0
+        self.x_pipe, self.y_pipe = 350, random.randint(self.rand_low, self.rand_high)
+        self.x_second_pipe, self.y_second_pipe = 576, random.randint(self.rand_low, self.rand_high)
+
         self.bird_img_sel = 0
         self.y_increase = 0.0
         self.pipes_stop = 0
         self.score_dozen, self.score_unity = 0, 0
         self.mouse_position = (0, 0)
+
         self.background_img=pygame.image.load('media/images/background-day.png')
         self.pipe_img_bottom = pygame.image.load('media/images/pipe-green-bottom.png')
         self.pipe_img_top = pygame.image.load('media/images/pipe-green-top.png')
@@ -36,20 +44,35 @@ class Game:
             self.bird_img = pygame.image.load('media/images/yellowbird-downflap.png')
         self.screen.blit(self.bird_img, (x, y))
 
-    def pipes(self, x, y):
-        """Simple function to create and move the obstacles, the pipes."""
-        y_top = y-(self.sizes['Pipe'][1]+self.space_between_pipes)
-        self.screen.blit(self.pipe_img_top, (x, y_top))
-        self.screen.blit(self.pipe_img_bottom, (x, y))
-        if self.x_pipe == -52:
-            self.x_pipe = 400
-            self.y_pipe = random.randint(150, 550)
-        if self.x_second_pipe == -52:
-            self.x_second_pipe = 400
-            self.y_second_pipe = random.randint(150, 550)
+    def pipes(self):
+        """Simple function to update the pipes positions."""
+
+        pipes_speed = 2
+
+        if self.x_pipe <= -self.img_sizes['Pipe'][0]:
+            self.x_pipe = self.screen_size[0]
+            self.y_pipe = random.randint(self.rand_low, self.rand_high)
+        if self.x_second_pipe <= -self.img_sizes['Pipe'][0]:
+            self.x_second_pipe = self.screen_size[0]
+            self.y_second_pipe = random.randint(self.rand_low, self.rand_high)
         elif self.pipes_stop == 0:
-            self.x_second_pipe -= 1
-            self.x_pipe -= 1
+            self.x_second_pipe -= pipes_speed
+            self.x_pipe -= pipes_speed
+
+        self.blit_pipes()
+
+    def blit_pipes(self):
+        """Blits the pipes to their current position."""
+
+        # Blit first pipe
+        y_top = self.y_pipe-(self.img_sizes['Pipe'][1]+self.space_between_pipes)
+        self.screen.blit(self.pipe_img_top, (self.x_pipe, y_top))
+        self.screen.blit(self.pipe_img_bottom, (self.x_pipe, self.y_pipe))
+
+        # Blit second pipe
+        y_top = self.y_second_pipe-(self.img_sizes['Pipe'][1]+self.space_between_pipes)
+        self.screen.blit(self.pipe_img_top, (self.x_second_pipe, y_top))
+        self.screen.blit(self.pipe_img_bottom, (self.x_second_pipe, self.y_second_pipe))
 
     def gameover(self):
         """Prints 'game over' when the bird hit the ground or the roof."""
@@ -114,9 +137,7 @@ class Game:
 
             self.y_increase += 0.25 #Simulates gravity acceleration
 
-            #Moves the pipes.
-            self.pipes(self.x_pipe, self.y_pipe)
-            self.pipes(self.x_second_pipe, self.y_second_pipe)
+            self.pipes() # Update pipes
 
             #Count the score.
             if self.x_bird > self.x_pipe+48 and self.x_bird < self.x_pipe+52:
@@ -134,16 +155,16 @@ class Game:
                 return 2
                 self.y_increase = 0
                 self.pipes_stop = 1
-            elif ((self.x_bird + self.sizes['Bird'][0])>=self.x_pipe+3)and(self.x_bird<=(self.x_pipe+50)):
-                if ((self.y_bird + self.sizes['Bird'][1])>=self.y_pipe)or(self.y_bird<=(self.y_pipe-self.space_between_pipes)):
+            elif ((self.x_bird + self.img_sizes['Bird'][0])>=self.x_pipe+3)and(self.x_bird<=(self.x_pipe+50)):
+                if ((self.y_bird + self.img_sizes['Bird'][1])>=self.y_pipe)or(self.y_bird<=(self.y_pipe-self.space_between_pipes)):
                     # Bird dies
                     self.gameover()
                     pygame.display.flip()
                     return 2
                     self.y_increase = 0
                     self.pipes_stop = 1
-            elif ((self.x_bird + self.sizes['Bird'][0])>=self.x_second_pipe+3)and(self.x_bird<=(self.x_second_pipe+50)):
-                if ((self.y_bird + self.sizes['Bird'][1])>=self.y_second_pipe)or(self.y_bird<=(self.y_second_pipe-self.space_between_pipes)):
+            elif ((self.x_bird + self.img_sizes['Bird'][0])>=self.x_second_pipe+3)and(self.x_bird<=(self.x_second_pipe+50)):
+                if ((self.y_bird + self.img_sizes['Bird'][1])>=self.y_second_pipe)or(self.y_bird<=(self.y_second_pipe-self.space_between_pipes)):
                     # Bird dies
                     self.gameover()
                     pygame.display.flip()
